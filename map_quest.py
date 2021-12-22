fba93e130fa4e987daea6e61fbab57500ecb7525
tholuongduc <tholuongducutc@gmail.com>
2021-12-22 15:57:40 +0000

    Map_Quest_Python

---
+++ map_quest.py
@@ -0,0 +1,42 @@
+#Replace "your_api_key" with your MapQuest API key
+import requests
+
+main_api = "https://www.mapquestapi.com/directions/v2/route?"
+key = "KouZWeGQmaBPRvtFbjoe0fqZhJABwS1a"
+
+while True:
+    orig = input("Starting Location: ")
+    if orig == "quit" or orig == "q":
+        break
+    dest = input("Destination: ")
+    if dest == "quit" or dest == "q":
+        break
+    request_url = main_api + "key=" + key + "&from=" + orig + "&to=" + dest
+    r = requests.get(request_url)
+    json_data = r.json()
+    print("URL: " + request_url)
+    json_status = json_data["info"]["statuscode"]
+    if json_status == 0:
+        print("API Status: " + str(json_status) + " = A successful route call.\n")
+        print("=============================================")
+        print("Directions from " + (orig) + " to " + (dest))
+        print("Trip Duration: " + (json_data["route"]["formattedTime"]))
+        print("Kilometers: " + str("{:.2f}".format((json_data["route"]["distance"])*1.61)))
+        print("Fuel Used (Ltr): " + str("{:.2f}".format((json_data["route"]["fuelUsed"])*3.78)))
+        print("=============================================")
+        for each in json_data["route"]["legs"][0]["maneuvers"]:
+            print((each["narrative"]) + " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)"))
+            print("=============================================\n")
+    elif json_status == 402:
+        print("**********************************************")
+        print("Status Code: " + str(json_status) + "; Invalid user inputs for one or both locations.")
+        print("**********************************************\n")
+    elif json_status == 611:
+        print("**********************************************")
+        print("Status Code: " + str(json_status) + "; Missing an entry for one or both locations.")
+        print("**********************************************\n")
+    else:
+        print("************************************************************************")
+        print("For Staus Code: " + str(json_status) + "; Refer to:")
+        print("https://developer.mapquest.com/documentation/directions-api/status-codes")
+        print("************************************************************************\n")
